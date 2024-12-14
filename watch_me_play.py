9eab1367fa7849fd4710e45999f66a414572433c
Alohamaan <131455740+Alohamaan@users.noreply.github.com>
2024-12-14 11:19:26 +0300

    Update watch_me_play.py

--- watch_me_play.py
+++ watch_me_play.py
@@ -1,5 +1,5 @@
 import os
-from final_nn_for_minesweeper.model_training.minesweeper_learner import MineSweeperLearner
+from model_training.minesweeper_learner import MineSweeperLearner
 import torch
 import numpy as np
 from model_code import model1
@@ -9,7 +9,7 @@ filterwarnings("ignore")
 np.set_printoptions(linewidth = 1000, precision=3, suppress=True)
 
 #Prompt user to specify the model they want to watch play
-preTrainedModels = os.listdir("trainedModels")
+preTrainedModels = os.listdir("pretrained_models")
 preTrainedModels = [i for i in preTrainedModels]
 preTrainedModels = np.sort(preTrainedModels)
 prompt = "Which model do you want to watch play? \n"
@@ -23,7 +23,7 @@ modelChoice = preTrainedModels[modelChoice - 1]
 dim1 = 16
 dim2 = 30
 model = model1.CustomModel(dim1, dim2)
-model.load_state_dict(torch.load(f'trainedModels/{modelChoice}'), strict=False)
+model.load_state_dict(torch.load(f'pretrained_models/{modelChoice}'), strict=False)
 model.eval()
 
 learner = MineSweeperLearner(modelChoice, model)
